9f36c5d501eb4542c7bf27e53923c6c79b56830a
IronySuzumiya <811073137@qq.com>
2023-04-18 19:01:27 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -65,44 +65,44 @@ sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
+  python3 -W ignore run_ginex.py --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
   > exp/ogbn-products/m4gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
   > exp/ogbn-products/m4gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
   > exp/ogbn-papers100M/m4gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
   > exp/ogbn-papers100M/m4gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 6400 --verbose \
+  python3 -W ignore run_ginex.py --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 6400 --verbose \
   > exp/soc-twitter-mpi-sws/m4gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
   > exp/soc-twitter-mpi-sws/m4gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --model=graphsage --sizes=10,10 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3600 --verbose \
+  python3 -W ignore run_ginex.py --model=graphsage --sizes=10,10 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 3600 --verbose \
   > exp/soc-friendster/m4gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:4gb \
-  python3 -W ignore run_ginex.py --sizes=-1 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 2000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
   > exp/soc-friendster/m4gb/gcn.out
 
 
@@ -113,42 +113,42 @@ sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
+  python3 -W ignore run_ginex.py --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 2100 --verbose \
   > exp/ogbn-products/m16gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 2100 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-products --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 2100 --verbose \
   > exp/ogbn-products/m16gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 3300 --verbose \
   > exp/ogbn-papers100M/m16gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=ogbn-papers100M --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 3300 --verbose \
   > exp/ogbn-papers100M/m16gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 6400 --verbose \
+  python3 -W ignore run_ginex.py --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 6400 --verbose \
   > exp/soc-twitter-mpi-sws/m16gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1,-1 --dataset=soc-twitter-mpi-sws --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 3300 --verbose \
   > exp/soc-twitter-mpi-sws/m16gb/gcn.out
 
 
 
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --model=graphsage --sizes=10,10 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3600 --verbose \
+  python3 -W ignore run_ginex.py --model=graphsage --sizes=10,10 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 3600 --verbose \
   > exp/soc-friendster/m16gb/graphsage.out
 
 sudo PYTHONPATH=/home/fpniu/.local/lib/python3.10/site-packages cgexec -g memory:16gb \
-  python3 -W ignore run_ginex.py --sizes=-1 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 6000000000 --feature-cache-size 2000000000 --sb-size 3300 --verbose \
+  python3 -W ignore run_ginex.py --sizes=-1 --dataset=soc-friendster --num-epochs=3 --neigh-cache-size 8000000000 --feature-cache-size 4000000000 --sb-size 3300 --verbose \
   > exp/soc-friendster/m16gb/gcn.out
